f3ea816afd4838edbbabe33bf808298795d5ed31
cesarsiguencia <cesar.siguencia@gmail.com>
2022-02-26 15:49:35 -0500

    finished adding methods and tests for Playeer

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,9 +1,9 @@
 const Potion = require('../lib/Potion.js');
 
 test('creates a health potion object',() => {
-    const potion = new Potion (); //this part is the constructor 
+    const potion = new Potion (); 
 
-    expect(potion.name).toEqual(expect.any(String));
+    expect(potion.name).toEqual(expect.any(String)); //this part is the constructor, the expect.any constructor is the argument. 
     expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
 })
